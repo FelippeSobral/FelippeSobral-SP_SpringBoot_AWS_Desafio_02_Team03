5485c71eae5e8b8b32259ade51a320b99d27ca70
vinnyhbr <113705995+vinnyhbr@users.noreply.github.com>
2024-10-29 18:24:46 -0300

    refactor: atualizando a classe discipline

--- challenge02/src/main/java/com/team03/challenge02/discipline/entity/Discipline.java
+++ challenge02/src/main/java/com/team03/challenge02/discipline/entity/Discipline.java
@@ -1,19 +1,22 @@
 package com.team03.challenge02.discipline.entity;
 
 import com.team03.challenge02.student.entity.Student;
+import com.team03.challenge02.teacher.entity.Teacher;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 @Entity
 @Table(name ="tb_disciplines")
-public class Discipline {
+public class Discipline implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -23,7 +26,15 @@ public class Discipline {
     @Column (name = "discription", nullable = false)
     private String discription;
 
-    //List<Student> students;
+    @JoinColumn(name = "full_teacher_id")
+    @ManyToOne
+    private Teacher fullTeacher;
+    @JoinColumn(name = "substitute_teacher_id")
+    @ManyToOne
+    private Teacher substituteTeacher;
+    @JoinColumn (name = "student_id")
+    @OneToMany
+    private List<Student> students = new ArrayList<>();
 
 
     @Override
