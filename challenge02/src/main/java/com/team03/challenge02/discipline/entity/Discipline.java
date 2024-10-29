6b191061528a802a740883cc19acd68c3d1a2d26
vinnyhbr <113705995+vinnyhbr@users.noreply.github.com>
2024-10-29 12:41:22 -0300

    refactor: completando a classe de disciplinas

--- challenge02/src/main/java/com/team03/challenge02/discipline/entity/Discipline.java
+++ challenge02/src/main/java/com/team03/challenge02/discipline/entity/Discipline.java
@@ -1,10 +1,14 @@
 package com.team03.challenge02.discipline.entity;
 
+import com.team03.challenge02.student.entity.Student;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.Objects;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 @Entity
@@ -12,5 +16,26 @@ import lombok.Setter;
 public class Discipline {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column (name = "discription", nullable = false)
+    private String discription;
+
+    //List<Student> students;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Discipline that = (Discipline) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
