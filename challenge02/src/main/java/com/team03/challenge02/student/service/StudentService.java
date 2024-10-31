e69b2ec45ed0daa4bfae6cdd86b3d4f0113ab9b6
Pablo Ernesto <pablo.guerreiro0308@hotmail.com>
2024-10-31 11:14:48 -0300

    feat: implementação do OpenFeign e Via Cep

--- challenge02/src/main/java/com/team03/challenge02/student/service/StudentService.java
+++ challenge02/src/main/java/com/team03/challenge02/student/service/StudentService.java
@@ -1,12 +1,42 @@
 package com.team03.challenge02.student.service;
 
+import com.team03.challenge02.student.dto.StudentDto;
+import com.team03.challenge02.student.entity.Student;
 import com.team03.challenge02.student.repository.StudentRepository;
+import com.team03.challenge02.student.viacep.ViaCepClient;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class StudentService {
-    //private final StudentRepository studentRepository;
+    @Autowired
+    private final StudentRepository studentRepository;
+    @Autowired
+    private ViaCepClient viaCepClient;
 
+    @Transactional
+    public Student save(Student student) {
+        if(student.getAdress() != null) {
+         var adress = viaCepClient.getAdress(student.getAdress());
+         String completeAdress = adress.street() + "," + adress.neighborhood()
+                                + "," + adress.city() + "," + adress.state();
+         student.setAdress(completeAdress);
+        }
+        return studentRepository.save(student);
+    }
+    @Transactional(readOnly = true)
+    public Student getById(Long id) {
+        return studentRepository.findById(id).orElseThrow(()
+                -> new EntityNotFoundException("Student not found with id " + id));
+    }
+    @Transactional(readOnly = true)
+    public List<Student> getAll() {
+        return studentRepository.findAll();
+    }
 }
