1bbe4bc1a12951c552ed8992d5e6489370b0ce82
Pablo Ernesto <pablo.guerreiro0308@hotmail.com>
2024-10-31 12:18:20 -0300

    feat: adicionado validação no StudentDto

--- challenge02/src/main/java/com/team03/challenge02/student/dto/StudentDto.java
+++ challenge02/src/main/java/com/team03/challenge02/student/dto/StudentDto.java
@@ -3,6 +3,7 @@ package com.team03.challenge02.student.dto;
 
 import com.team03.challenge02.course.entity.Course;
 import com.team03.challenge02.roles.Role;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -12,8 +13,13 @@ import java.time.LocalDate;
 public class StudentDto {
 
     private Long id;
+
+    @Pattern(regexp = "^[A-Z].*", message = "First name must start with an uppercase letter")
     private String firstName;
+
+    @Pattern(regexp = "^[A-Z].*", message = "Last name must start with an uppercase letter")
     private String lastName;
+
     private String email;
     private LocalDate birthDate;
     private String course;
