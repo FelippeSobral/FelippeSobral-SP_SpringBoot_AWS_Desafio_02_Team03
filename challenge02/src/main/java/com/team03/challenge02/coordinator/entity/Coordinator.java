43be45eaf7a706926d1fc27b87fd97f02ae367f9
Felipe Moreira <felipe0365@gmail.com>
2024-11-01 10:35:37 -0300

    fix: corrigi a classe Coordinator

--- challenge02/src/main/java/com/team03/challenge02/coordinator/entity/Coordinator.java
+++ challenge02/src/main/java/com/team03/challenge02/coordinator/entity/Coordinator.java
@@ -11,27 +11,6 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 
-@Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(callSuper = true, of = "id")
+
 public class Coordinator extends Person implements Serializable {
-
-    private Course course;
-
-    public Coordinator() {
-    }
-
-    public Coordinator(String firstName, String lastName, String email, LocalDate birthDate, Course course) {
-        super(firstName, lastName, email, birthDate);
-        this.course = course;
-    }
-
-    public void setCourse(Course course) {
-        this.course = course;
-    }
-
-
 }
