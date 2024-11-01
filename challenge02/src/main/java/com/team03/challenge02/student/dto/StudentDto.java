bb104b96723157795d9b5bc6eebe4737e1af96c6
Pablo Ernesto <pablo.guerreiro0308@hotmail.com>
2024-11-01 00:43:51 -0300

    refactor: refatoração do StudentDto

--- challenge02/src/main/java/com/team03/challenge02/student/dto/StudentDto.java
+++ challenge02/src/main/java/com/team03/challenge02/student/dto/StudentDto.java
@@ -3,28 +3,31 @@ package com.team03.challenge02.student.dto;
 
 import com.team03.challenge02.course.entity.Course;
 import com.team03.challenge02.roles.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.LocalDate;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
-@ToString
-public class StudentDto {
+@Getter @Setter
+public record StudentDto (
 
-    private Long id;
-
-    @Pattern(regexp = "^[A-Z].*", message = "First name must start with an uppercase letter")
-    private String firstName;
-
-    @Pattern(regexp = "^[A-Z].*", message = "Last name must start with an uppercase letter")
-    private String lastName;
-
-    private String email;
-    private LocalDate birthDate;
-    private String course;
-    private String adress;
-    private String role;
-
-
+     Long id,
+     @NotBlank(message = "First name is mandatory")
+     @Pattern(regexp = "^[A-Z].*", message = "First name must start with an uppercase letter")
+     String firstName,
+     @NotBlank(message = "First name is mandatory")
+     @Pattern(regexp = "^[A-Z].*", message = "Last name must start with an uppercase letter")
+     String lastName,
+     @NotBlank(message = "Email is mandatory")
+     @Email(message = "Email should be valid")
+     @Pattern(regexp = "^[\\w-\\.]+@[\\w-]+\\.[a-zA-Z]{2,}$")
+     String email,
+     LocalDate birthDate,
+     String course,
+     String adress,
+     String role
+     )
+{
 }
