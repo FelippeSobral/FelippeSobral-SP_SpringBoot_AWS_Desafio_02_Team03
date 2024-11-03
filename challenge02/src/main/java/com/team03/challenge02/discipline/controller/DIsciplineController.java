1cfe93153a4e3d3649cc7ed346da8a85ddffc251
vinnyhbr <113705995+vinnyhbr@users.noreply.github.com>
2024-11-03 15:30:14 -0300

    feat: metodo de findAll e delete da entidade discipline

--- challenge02/src/main/java/com/team03/challenge02/discipline/controller/DIsciplineController.java
+++ challenge02/src/main/java/com/team03/challenge02/discipline/controller/DIsciplineController.java
@@ -1,9 +1,31 @@
 package com.team03.challenge02.discipline.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.team03.challenge02.course.entity.Course;
+import com.team03.challenge02.discipline.entity.Discipline;
+import com.team03.challenge02.discipline.services.DisciplineServices;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/disciplines")
 public class DIsciplineController {
 
-    
+    public final DisciplineServices disciplineServices;
+
+    public DIsciplineController(DisciplineServices disciplineServices) {
+        this.disciplineServices = disciplineServices;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Discipline>> findAll(){
+        List<Discipline> list = disciplineServices.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        disciplineServices.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- challenge02/src/main/java/com/team03/challenge02/discipline/services/DisciplineServices.java
+++ challenge02/src/main/java/com/team03/challenge02/discipline/services/DisciplineServices.java
@@ -1,31 +1,33 @@
 package com.team03.challenge02.discipline.services;
 
+import com.team03.challenge02.course.entity.Course;
 import com.team03.challenge02.discipline.dto.DisciplineDto;
 import com.team03.challenge02.discipline.entity.Discipline;
 import com.team03.challenge02.discipline.repository.DisciplineRepository;
+import com.team03.challenge02.exception.NameUniqueViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class DisciplineServices {
 
     private final DisciplineRepository disciplineRepository;
-    //private final Course
-
-    public Discipline create(Discipline discipline) {
-        return disciplineRepository.save(discipline);
-    }
 
     public DisciplineServices(DisciplineRepository disciplineRepository) {
         this.disciplineRepository = disciplineRepository;
     }
 
-    public DisciplineDto postDiscipline(DisciplineDto disciplineDto){
-        return disciplineDto;
+    public List<Discipline> findAll(){
+        return disciplineRepository.findAll();
     }
 
     public Optional<Discipline> getById(long id) {
         return disciplineRepository.findById(id);
+    }
+    public void delete(Long id){
+        disciplineRepository.deleteById(id);
     }
 }
