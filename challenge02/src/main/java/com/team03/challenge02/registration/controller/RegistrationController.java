9101251493d07530bbdf6645b887a984d1b4dd54
FelippeSobral <lippesobral@hotmail.com>
2024-11-03 14:24:10 -0300

    inserção do controller da classe registration

--- challenge02/src/main/java/com/team03/challenge02/registration/controller/RegistrationController.java
+++ challenge02/src/main/java/com/team03/challenge02/registration/controller/RegistrationController.java
@@ -1,10 +1,48 @@
 package com.team03.challenge02.registration.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.team03.challenge02.registration.Services.RegistrationServices;
+import com.team03.challenge02.registration.dto.RegistrationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/Registration")
 public class RegistrationController {
+
+@Autowired
+private RegistrationServices registrationServices;
+
+@PostMapping
+@PreAuthorize("hasRole('ROLE_COORDINATOR')")
+public ResponseEntity<RegistrationDTO> createRegistration(@RequestBody RegistrationDTO registrationDTO) {
+    RegistrationDTO newRegistration = registrationServices.createRegistration(registrationDTO.student_id(), registrationDTO.course_id());
+    return ResponseEntity.status(HttpStatus.CREATED).body(newRegistration);
+}
+
+@GetMapping("studant/{id}")
+public ResponseEntity<List<RegistrationDTO>> listRegistrations(@PathVariable Long student_id) {
+    List<RegistrationDTO> registrations = registrationServices.listRegistrationsByStudent(student_id);
+    return ResponseEntity.ok(registrations);
+}
+
+@DeleteMapping("/{id}")
+@PreAuthorize("hasRole('ROLE_COORDINATOR')")
+public ResponseEntity<Void> deleteRegistration(@PathVariable Long id) {
+    registrationServices.deleteRegistration(id);
+    return ResponseEntity.noContent().build();
+}
+
+
+
+
+
+
+
+
 }
--- challenge02/src/main/java/com/team03/challenge02/registration/dto/RegistrationDTO.java
+++ challenge02/src/main/java/com/team03/challenge02/registration/dto/RegistrationDTO.java
@@ -1,4 +1,4 @@
 package com.team03.challenge02.registration.dto;
 
-public record RegistrationDTO(Long id, Long alunoId, Long cursoId) {
+public record RegistrationDTO(Long id, Long student_id, Long course_id) {
 }
