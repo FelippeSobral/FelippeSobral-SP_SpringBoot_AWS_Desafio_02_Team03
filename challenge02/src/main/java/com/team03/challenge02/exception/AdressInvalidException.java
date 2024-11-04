b2a6850c290e28b26321ba5f282b1d784feaa552
Pablo Ernesto <pablo.guerreiro0308@hotmail.com>
2024-11-04 12:05:58 -0300

    refactor: alteração nas exceções relacionadas a Adress

--- challenge02/src/main/java/com/team03/challenge02/exception/AdressInvalidException.java
+++ challenge02/src/main/java/com/team03/challenge02/exception/AdressInvalidException.java
@@ -1,4 +1,4 @@
-package com.team03.challenge02.student.exception;
+package com.team03.challenge02.exception;
 
 public class AdressInvalidException extends RuntimeException {
     public AdressInvalidException(String message) {
